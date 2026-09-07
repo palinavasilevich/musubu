1eb37d11ab8d14ae44c7d8ccd1b553c952b70767
pvslvch <polina.vasilevichh@gmail.com>
2026-09-07 16:51:24 +0200

    feat(fix): fix project image upload

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
         hostname: "*.ufs.sh",
       },
     ],
